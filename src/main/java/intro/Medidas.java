8d3689b1b0537c0396905f56d4faca14d004c89c
Edvaldo Silva <eddy.silva32@gmail.com>
2021-06-05 11:14:49 -0300

    Criei o método calcularAreaModoCurto e movi o código que estava em main para lá.
    O método main agora chama os dois métodos calcular* que passaram também a serem estáticos.

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,50 @@
+// 1 - Pacote
+package intro;
+
+
+// 2 - Referência às Bibliotecas
+
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Características
+
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+
+
+    }
+
+    public static void calcularAreaModoCurto(){
+        System.out.println("Cálculo de Areas Modo Curto");
+
+        // Cálculo de área - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de " + largura * comprimento + "m²");
+    }
+
+    public static void calcularAreaModoExtenso(){
+        System.out.println("Cálculo de Areas Modo Extenso");
+
+        // Cálculo de área - Exemplo: o tamanho do tapaete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; // Largura recebe 4 unidades
+        comprimento = 6;
+
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de " + resultado + "m²");
+
+    }
+
+
+}
