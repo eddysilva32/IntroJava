454c75d209efdb1cbe7bcc684f5aa3e3c1556185
Edvaldo Silva <eddy.silva32@gmail.com>
2021-06-05 13:12:58 -0300

    Aula 2 do curso introdução ao Java;
    Criação de if, switch e For.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -5,18 +5,80 @@ package intro;
 // 2 - Referência às Bibliotecas
 
 
+import java.util.Scanner;
+
 // 3 - Classe
 public class Medidas {
     // 3.1 - Atributos - Características
 
 
     // 3.2 - Métodos e Funções
-    public static void main(String[] args){
-        calcularAreaModoCurto();
-        calcularAreaModoExtenso();
+    public static void main(String[] args) {
+        // Condicional = Verficar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
 
+        // Utilizar a classe Scanner do Java para ler a escolha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U  D E  O P Ç Ô E S");
+        System.out.println("C - Calcular Area modo Curto");
+        System.out.println("d - Contar até Dez");
+        System.out.println("e - Calcular Area modo Extenso");
+        System.out.println("i - If Simples");
+        System.out.println("r - Contagem regressiva de 10 até 0");
+        System.out.println("Digite a opção desejada: ");
+        String opcao = scanner.next();
+
+
+        // switch = selecionar o comportamento  do programa conforme a scolha da pessoa ou do software
+
+        switch (opcao){
+            case "c":
+            case "C":
+                System.out.println("Você escolheu executar o método calcularAreaModoCurto");
+                calcularAreaModoCurto();
+                break;
+            case "d":
+            case "D":
+                System.out.println("Você escolheu executar o método contarAteDez");
+                contarAteDez();
+                break;
+            case "e":
+            case "E":
+                System.out.println("Você escolheu executar o método calcularAreaModoExtenso");
+                calcularAreaModoExtenso();
+                break;
+            case "i":
+            case "I":
+                System.out.println("Você escolheu executar o método ifSimples");
+                ifSimples();
+                break;
+            case "r":
+            case "R":
+                System.out.println("Você escolheu executar o método contagemRegressiva");
+                contagemRegressiva();
+                break;
+            default:
+                System.out.println("Você escolheu outro valor que não tem uma ação associada");
+
+        }
 
     }
+        public static void ifSimples(){
+            // Condicional = Verficar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
+
+            // if = se
+            // else = senão
+
+            String modo = "curto";
+
+            if (modo ==  "curto"){
+                calcularAreaModoCurto();
+            }
+            else{
+                calcularAreaModoExtenso();
+            }
+
+        }
+
 
     public static void calcularAreaModoCurto(){
         System.out.println("Cálculo de Areas Modo Curto");
@@ -46,5 +108,24 @@ public class Medidas {
 
     }
 
+    public static void contarAteDez(){
+
+        // Loops ou Repetições
+        // for = repetição incondicional
+
+        System.out.println("Contagem Crescente");
+        for(int numero = 1; numero <= 10; numero++){
+            System.out.println(numero);
+        }
+
+    }
+
+    public static void contagemRegressiva() {
+        System.out.println("Contagem Regressiva");
+
+        for (int numero = 10; numero > -1; numero--) {
+            System.out.println(numero);
+        }
+    }
 
 }
